ae86d8c67acde7f2b028a611ce43506f4e9e3547
Aldo Wanderley Costa Junior <aldowanderley11@gmail.com>
2024-01-27 15:47:19 -0300

    :sparkles: feat: creating register page

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,21 +1,28 @@
 import Image from "next/image";
 import TeamDiscussion from '../../public/team-discussion.png'
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/router";
 
 interface WrapperProps {
   children: React.ReactNode;
 }
 
 export const Wrapper = ({ children }: WrapperProps) => {
+  const pathName = usePathname()
+  const isRegister = pathName === '/register'
+
   return (
-    <section className="flex items-center justify-center h-screen md:py-0 px-3">
+    <section className="flex items-center justify-center md:h-screen md:px-0 px-3 md:py-0 py-3 ">
       <div className="border rounded-lg border-[#282828] shadow-xl md:p-9 p-5">
         <p className="text-2xl text-[#000] font-light mb-7">Welcome !</p>
-        <h1 className="text-3xl font-medium text-[#000] mb-1">Sign in to</h1>
+        <h1 className="text-3xl font-medium text-[#000] mb-1">Sign {isRegister ? "out" : "in"} to</h1>
         <h2 className="text-base font-normal text-[#000] mb-12">Lorem Ipsum is simply</h2>
         { children }
-        <Link href='/' className="text-[#7D7D7D] text-base font-light mt-14 text-center block">
-          Don’y have an Account ? <span className="text-[#000] font-semibold">Register</span>
+        <Link href={ isRegister ? '/' : '/register' } className="text-[#7D7D7D] text-base font-light mt-14 text-center block">
+          { isRegister ? "Already have an Account ?" : "Don’y have an Account ?"} <span className="text-[#000] font-semibold">
+            { isRegister ? "Sign in" : "Sign up"}
+          </span>
         </Link>
       </div>
       <Image
